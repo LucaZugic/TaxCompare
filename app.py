ddfba9dea3d43386935546c79ced3dbcd6b0e781
LucaZugic <zugic.luca@gmail.com>
2021-05-07 12:08:48 +0100

    implementing multi-currency comparison

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ convert = CurrencyRates().convert
 @app.route('/', methods=['GET'])
 def index():
     success = 'alert-lower'
-    return render_template('index.html')
+    return render_template('index.html', selected="selected")
 
 
 @app.route('/submit', methods=['POST'])
@@ -55,54 +55,140 @@ def compute():
                 elif region[i] == 'Eng1':
                     ni_extension[i] = "& Nation Insurance"
                     if currency != 'GBP':
-                        burden[i] = convert('GBP', currency[i], (compute_tax_england(convert(currency[i], 'GBP', income)) + compute_ni(convert(currency[i], 'GBP', income))))
+                        burden[i] = convert('GBP', currency[i], compute_tax_england(convert(currency[i], 'GBP', income)) + compute_ni(convert(currency[i], 'GBP', income)))
                     else:
                         burden[i] = compute_tax_england(income) + compute_ni(income)
+                elif region[i] == 'Ger':
+                    if currency != 'EUR':
+                        burden[i] = convert('GBP', currency[i], compute_tax_germany(convert(currency[i], 'GBP', income)))
+                    else:
+                        burden[i] = compute_tax_germany(income)
             
                 net_income[i] = round(income - burden[i], 2)
                 percentage_burden [i] = round((burden[i] / income) * 100, 2)
 
         if has_income[0] and has_income[1]:
 
-            net_income_difference = net_income[1] - net_income[0]
-
-            if net_income_difference < 0:
-                income_alert[0] = alert_types[0]
-                income_alert[1] = alert_types[1]
-            elif net_income_difference > 0:
-                income_alert[0] = alert_types[1]
-                income_alert[1] = alert_types[0]
-            else:
-                income_alert[0] = alert_types[2]
-                income_alert[1] = alert_types[2]
-
-            tax_percentage_difference = percentage_burden[1] - percentage_burden[0]
-            
-            if tax_percentage_difference < 0:
-                perc_alert[0] = alert_types[1]
-                perc_alert[1] = alert_types[0]
-            elif tax_percentage_difference > 0:
-                perc_alert[0] = alert_types[0]
-                perc_alert[1] = alert_types[1]
-            else:
-                perc_alert[0] = alert_types[2]
-                perc_alert[1] = alert_types[2]
-
-            tax_difference = burden[1] - burden[0]
-
-            if tax_difference < 0:
-                tax_alert[0] = alert_types[1]
-                tax_alert[1] = alert_types[0]
-            elif tax_difference > 0:
-                tax_alert[0] = alert_types[0]
-                tax_alert[1] = alert_types[1]
-            else:
-                tax_alert[0] = alert_types[2]
-                tax_alert[1] = alert_types[2]
-
             if currency[0] != currency[1]:
-                pass
+                burden_converted = [0, 0]
+                net_income_converted = [0, 0]
+                burden_converted = [0, 0]
+                tax_difference = [0, 0]
+                net_income_converted[0] = convert(currency[0], currency[1], net_income[0])
+                net_income_converted[1] = convert(currency[1], currency[0], net_income[1])
+                net_income_difference_1 = net_income_converted[1] - net_income[0]
+                net_income_difference_2 = net_income[1] - net_income_converted[0]
+                burden_converted[0] = convert(currency[0], currency[1], burden[0])
+                burden_converted[1] = convert(currency[1], currency[0], burden[1])
+                tax_difference[0] = burden_converted[1] - burden[0]
+                tax_difference[1] = burden[1] - burden_converted[0]
+                
+
+                if net_income_difference_1 < 0:
+                    income_alert[0] = alert_types[0]
+                    income_alert[1] = alert_types[1]
+                elif net_income_difference_1 > 0:
+                    income_alert[0] = alert_types[1]
+                    income_alert[1] = alert_types[0]
+                else:
+                    income_alert[0] = alert_types[2]
+                    income_alert[1] = alert_types[2]
+
+                tax_percentage_difference = percentage_burden[1] - percentage_burden[0]
+
+                if tax_percentage_difference < 0:
+                    perc_alert[0] = alert_types[1]
+                    perc_alert[1] = alert_types[0]
+                elif tax_percentage_difference > 0:
+                    perc_alert[0] = alert_types[0]
+                    perc_alert[1] = alert_types[1]
+                else:
+                    perc_alert[0] = alert_types[2]
+                    perc_alert[1] = alert_types[2]
+
+                tax_difference = burden[1] - burden[0]
+
+                if tax_difference < 0:
+                    tax_alert[0] = alert_types[1]
+                    tax_alert[1] = alert_types[0]
+                elif tax_difference > 0:
+                    tax_alert[0] = alert_types[0]
+                    tax_alert[1] = alert_types[1]
+                else:
+                    tax_alert[0] = alert_types[2]
+                    tax_alert[1] = alert_types[2]
+
+                return render_template('index.html',
+                    # Income 1:
+                    income_alert_1=income_alert[0],
+                    annual_net_income_1=net_income[0],
+                    annual_net_income_1_converted=" = " + str(round(net_income_converted[0], 2)),
+                    monthly_net_income_1=round(net_income[0] / 12, 2),
+                    monthly_net_income_1_converted=" = " + str(round(net_income_converted[0] / 12, 2)),
+                    cur1=currency[0],
+                    cur1_conv=currency[1],
+                    ni_extension_1=ni_extension[0],
+                    tax_alert_1=tax_alert[0],
+                    percentage_alert_1=perc_alert[0],
+                    percentage_tax_1=str(percentage_burden[0]) + " %",
+                    annual_tax_1=round(burden[0], 2),
+                    monthly_tax_1=round(burden[0] / 12, 2),
+                    # Differences:
+                    annual_net_difference_1=abs(net_income[1] - net_income[0]),
+                    monthly_net_difference_1=round(abs(net_income[1] - net_income[0]) / 12, 2),
+                    percentages_difference=str(round(abs(percentage_burden[1] - percentage_burden[0]), 2)) + " %",
+                    annual_tax_difference=round(abs(burden[1] - burden[0]), 2),
+                    monthly_tax_difference=round(abs(burden[1] - burden[0]) / 12, 2),
+                    difference_currency_1=currency[0],
+                    # Income 2:
+                    income_alert_2=income_alert[1],
+                    annual_net_income_2=net_income[1],
+                    monthly_net_income_2=round(net_income[1] / 12, 2),
+                    cur2=currency[1],
+                    ni_extension_2=ni_extension[1],
+                    tax_alert_2=tax_alert[1],
+                    percentage_alert_2=perc_alert[1],
+                    percentage_tax_2=str(percentage_burden[1]) + " %",
+                    annual_tax_2=round(burden[1], 2),
+                    monthly_tax_2=round(burden[1] / 12, 2))
+                    
             else:
+                net_income_difference = net_income[1] - net_income[0]
+
+                if net_income_difference < 0:
+                    income_alert[0] = alert_types[0]
+                    income_alert[1] = alert_types[1]
+                elif net_income_difference > 0:
+                    income_alert[0] = alert_types[1]
+                    income_alert[1] = alert_types[0]
+                else:
+                    income_alert[0] = alert_types[2]
+                    income_alert[1] = alert_types[2]
+
+                tax_percentage_difference = percentage_burden[1] - percentage_burden[0]
+
+                if tax_percentage_difference < 0:
+                    perc_alert[0] = alert_types[1]
+                    perc_alert[1] = alert_types[0]
+                elif tax_percentage_difference > 0:
+                    perc_alert[0] = alert_types[0]
+                    perc_alert[1] = alert_types[1]
+                else:
+                    perc_alert[0] = alert_types[2]
+                    perc_alert[1] = alert_types[2]
+
+                tax_difference = burden[1] - burden[0]
+
+                if tax_difference < 0:
+                    tax_alert[0] = alert_types[1]
+                    tax_alert[1] = alert_types[0]
+                elif tax_difference > 0:
+                    tax_alert[0] = alert_types[0]
+                    tax_alert[1] = alert_types[1]
+                else:
+                    tax_alert[0] = alert_types[2]
+                    tax_alert[1] = alert_types[2]
+
                 return render_template('index.html',
                     # Income 1:
                     income_alert_1=income_alert[0],
@@ -116,12 +202,12 @@ def compute():
                     annual_tax_1=round(burden[0], 2),
                     monthly_tax_1=round(burden[0] / 12, 2),
                     # Differences:
-                    annual_net_difference=abs(net_income[1] - net_income[0]),
-                    monthly_net_difference=round(abs(net_income[1] - net_income[0]) / 12, 2),
+                    annual_net_difference_1=abs(net_income[1] - net_income[0]),
+                    monthly_net_difference_1=round(abs(net_income[1] - net_income[0]) / 12, 2),
                     percentages_difference=str(round(abs(percentage_burden[1] - percentage_burden[0]), 2)) + " %",
-                    annual_tax_difference=round(abs(burden[1] - burden[0]), 2),
-                    monthly_tax_difference=round(abs(burden[1] - burden[0]) / 12, 2),
-                    difference_currency=currency[0],
+                    annual_tax_difference_1=round(abs(burden[1] - burden[0]), 2),
+                    monthly_tax_difference_1=round(abs(burden[1] - burden[0]) / 12, 2),
+                    difference_currency_1=currency[0],
                     # Income 2:
                     income_alert_2=income_alert[1],
                     annual_net_income_2=net_income[1],
@@ -152,23 +238,36 @@ def about():
     return render_template('about.html')
 
 
+def compute_tax_germany(income):
+    if income <= 9408:
+        return 0
+    elif income <= 14532:
+        return (.14 + (income - 9408) * .0000097287) * (income-9408)
+    elif income <=  57051:
+        return (.2397 + (income-14532) * .0000021202) * (income - 14532) + 972.79
+    elif income <= 270500:
+        return .42 * income - 8963.74
+    else:
+        return .45 * income - 17078.74
+
+
 def compute_tax_scotland(income):
     if income <= 12570:
         return 0
     elif income <= 14667:
         return (income - 12570) * .19
     elif income <= 25296:
-        return (income - 14667) * .2 + (14667 - 12570) * .19
+        return (income - 14667) * .2 + 398.43
     elif income <= 43663:
-        return (income - 25296) * .21 + (25296 - 14667) * .2 + (14667 - 12570) * .19
+        return (income - 25296) * .21 + 2524.23
     elif income <= 100000:
-        return (income - 43663) * .41 + (43663 - 25296) * .21 + (25296 - 14667) * .2 + (14667 - 12570) * .19
+        return (income - 43663) * .41 + 6381.3
     elif income <= 125140:
-        return (income - 43663) * .41 + (43663 - 25296) * .21 + (25296 - 14667) * .2 + (14667 - (12570 - (income-100000)/2)) * .19
+        return (income - 43663) * .41 + 5982.87 + (14667 - (12570 - (income-100000)/2)) * .19
     elif income <= 150000:
-        return (income - 43663) * .41 + (43663 - 25296) * .21 + (25296 - 14667) * .2 + 14667 * .19
+        return (income - 43663) * .41 + 8769.6
     else:
-        return (income - 150000) * .46 +(150000 - 43663) * .41 + (43663 - 25296) * .21 + (25296 - 14667) * .2 + 14667 * .19
+        return (income - 150000) * .46 + 52367.77
 
 
 def compute_tax_england(income):
@@ -177,13 +276,13 @@ def compute_tax_england(income):
     elif income <= 50270:
         return (income - 12570) * .2
     elif income <= 100000:
-        return (income - 50270) * .4 + (50270 - 12570) * .2
+        return (income - 50270) * .4 + 7540
     elif income <= 125140:
         return (income - 50270) * .4 + (50270 - (12570 - (income-100000)/2)) * .2
     elif income <= 150000:
-        return (income - 50270) * .4 + (43663 - 25296) * .21 + 25296 * .2
+        return (income - 50270) * .4 + 8916.27
     else:
-        return (income - 150000) * .45 + (150000 - 50270) * .4 + (50270 - 25296) * .2
+        return (income - 150000) * .45 + 44886.8
 
 
 def compute_ni(income):
@@ -193,7 +292,7 @@ def compute_ni(income):
     elif monthly_income <= 4189:
         return (monthly_income - 797) * .12 * 12
     else:
-        return ((4189 - 797) * .12 + (monthly_income - 4189) * .02) * 12
+        return (407.04 + (monthly_income - 4189) * .02) * 12
 
 
 if __name__ == "__main__":
